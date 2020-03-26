8e0ee2d6e6a5659cc4e1560d952dd583671549e9
Andy-thor <andy.dev536@gmail.com>
2020-03-26 19:07:04 -0300

    .

--- resources/js/ajax.js
+++ resources/js/ajax.js
@@ -35,57 +35,59 @@ function loadJSON(path) {
 function loadSpecDownload() {
 	var xhr = new XMLHttpRequest();
 	xhr.open("GET", "../resources/data.json", true);
-	xhr.onreadystatechange = function(){
-		var data = JSON.parse(xhr.responseText);
-		alert("DATA" + JSON.stringify(data))
-		if (data) {
-			var repo = data["project-name"],
-				developer = data["developer"],
-				version = data["version"],
-				lang = getLanguage(),
-				idLang = 0,
-				currentOS = getOS();
+	xhr.onreadystatechange = function() {
+		if(this.readyState === 4 && this.status === 200) {
+			var data = JSON.parse(this.responseText);
+			alert("DATA" + JSON.stringify(data))
+			if (data) {
+				var repo = data["project-name"],
+					developer = data["developer"],
+					version = data["version"],
+					lang = getLanguage(),
+					idLang = 0,
+					currentOS = getOS();
 
-			var filename = "";
-			var filesize = "";
-			var urlDownload = "";
-			console.log(data);
-			if (lang === "es") {
-				idLang = 1;
-			}
-			var project_name = data["project-name"];
-			if (currentOS == "Windows") {
-				var project_name_lower = repo.toLowerCase();
-				filename = data["latest-release"]["exe"]["filename"]
-						   .replace("{project-name}", repo.toLowerCase())
-						   .replace("{version}", version);
-				filesize = data["latest-release"]["exe"]["filesize"];
-				urlDownload = data["latest-release"]["exe"]["url"]
-							  .replace("{developer}", developer)
-							  .replace("{project-name}", repo)
-							  .replace("{version}", version)
-							  .replace("{filename}", filename);
-			} else {
-				filename = data["latest-release"]["zipball"]["filename"]
-						   .replace("{project-name}", repo)
-						   .replace("{version}", version);
-				filesize = data["latest-release"]["zipball"]["filesize"];
-				urlDownload = data["latest-release"]["zipball"]["url"]
-							  .replace("{developer}", developer)
-							  .replace("{project-name}", repo)
-							  .replace("{version}", version);
-			}
-			
-			var textDownload = data["text"]["labels-spec"]["download"][idLang]
+				var filename = "";
+				var filesize = "";
+				var urlDownload = "";
+				console.log(data);
+				if (lang === "es") {
+					idLang = 1;
+				}
+				var project_name = data["project-name"];
+				if (currentOS == "Windows") {
+					var project_name_lower = repo.toLowerCase();
+					filename = data["latest-release"]["exe"]["filename"]
+							   .replace("{project-name}", repo.toLowerCase())
+							   .replace("{version}", version);
+					filesize = data["latest-release"]["exe"]["filesize"];
+					urlDownload = data["latest-release"]["exe"]["url"]
+								  .replace("{developer}", developer)
+								  .replace("{project-name}", repo)
+								  .replace("{version}", version)
+								  .replace("{filename}", filename);
+				} else {
+					filename = data["latest-release"]["zipball"]["filename"]
 							   .replace("{project-name}", repo)
 							   .replace("{version}", version);
-			$("a.button-download").attr("href", urlDownload);
-			$("a.button-download").html(textDownload);
-			$(".body-spec p#file-name").html("<span class='bold-text'>" + data["text"]["labels-spec"]["filename"][idLang] + ":</span> " + filename);
-			$(".body-spec p#file-size").html("<span class='bold-text size-bytes'>" + data["text"]["labels-spec"]["filesize"][idLang] + ":</span> " + filesize);
-			$(".body-spec p#platform").html("<span class='bold-text'>" + data["text"]["labels-spec"]["platform"][idLang] + ":</span> " + currentOS);
-		} else {
-			console.log("Error loading file");
+					filesize = data["latest-release"]["zipball"]["filesize"];
+					urlDownload = data["latest-release"]["zipball"]["url"]
+								  .replace("{developer}", developer)
+								  .replace("{project-name}", repo)
+								  .replace("{version}", version);
+				}
+				
+				var textDownload = data["text"]["labels-spec"]["download"][idLang]
+								   .replace("{project-name}", repo)
+								   .replace("{version}", version);
+				$("a.button-download").attr("href", urlDownload);
+				$("a.button-download").html(textDownload);
+				$(".body-spec p#file-name").html("<span class='bold-text'>" + data["text"]["labels-spec"]["filename"][idLang] + ":</span> " + filename);
+				$(".body-spec p#file-size").html("<span class='bold-text size-bytes'>" + data["text"]["labels-spec"]["filesize"][idLang] + ":</span> " + filesize);
+				$(".body-spec p#platform").html("<span class='bold-text'>" + data["text"]["labels-spec"]["platform"][idLang] + ":</span> " + currentOS);
+			} else {
+				console.log("Error loading file");
+			}
 		}
 	};
 	xhr.send();
