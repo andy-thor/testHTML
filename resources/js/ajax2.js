ca874bb8b34967b791f3f623d4f60142fa42af6e
Andy-thor <andy.dev536@gmail.com>
2020-03-26 15:59:29 -0300

    .

--- resources/js/ajax2.js
+++ resources/js/ajax2.js
@@ -64,8 +64,8 @@ function startAjax() {
 				}
 				alert("BEFORE JQUERY");
 				let textDownload = data["text"]["labels-spec"]["download"]
-								   .replace("{project-name}", repo)
-								   .replace("{version}", version);
+				textDownload = textDownload.replace("{project-name}", repo)
+							   .replace("{version}", version);
 				var test1 = data["text"]["labels-spec"]["filename"][idLang];
 				var test2 = data["text"]["labels-spec"]["filesize"][idLang];
 
