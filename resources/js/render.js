004a35032ad838164745b693a52dd04c2c83a1f3
Andy-thor <andy.dev536@gmail.com>
2020-03-27 15:37:33 -0300

    Added IDs

--- resources/js/render.js
+++ resources/js/render.js
@@ -1,3 +1,40 @@
+function funcionRender() {
+	var elements = document.getElementsByTagName('*'),
+		i;
+	for (i in elements) {
+		if (elements[i].hasAttribute && elements[i].hasAttribute('data-include')) {
+			fragment(elements[i], elements[i].getAttribute('data-include'));
+		}
+	}
+	function fragment(el, url) {
+		var localTest = /^(?:file):/,
+			xmlhttp = new XMLHttpRequest(),
+			status = 0;
+
+		xmlhttp.onreadystatechange = function() {
+			/* if we are on a local protocol, and we have response text, we'll assume
+ *  				things were sucessful */
+			if (xmlhttp.readyState == 4) {
+				status = xmlhttp.status;
+			}
+			if (localTest.test(location.href) && xmlhttp.responseText) {
+				status = 200;
+			}
+			if (xmlhttp.readyState == 4 && status == 200) {
+				el.outerHTML = xmlhttp.responseText;
+			}
+		}
+
+		try {
+			xmlhttp.open("GET", url, true);
+			xmlhttp.send();
+		} catch(err) {
+			alert("ERROR EN URL: " + url);
+			/* todo catch error */
+		}
+	}
+}
+
 function includeHTML() {
   var z, i, elmnt, file, xhttp;
 	/*loop through a collection of all HTML elements:*/
@@ -5,6 +42,7 @@ function includeHTML() {
 	for (i = 0; i < z.length; i++) {
 		elmnt = z[i];
 		/*search for elements with a certain atrribute:*/
+		alert("EL ID ES: " + elmnt.querySelector("#id"));
 		file = elmnt.getAttribute("w3-include-html");
 		if (file) {
 			/*make an HTTP request using the attribute value as the file name:*/
@@ -17,7 +55,7 @@ function includeHTML() {
 				elmnt.removeAttribute("w3-include-html");
 				includeHTML();
 			}
-		}      
+		}
 		xhttp.open("GET", file, true);
 		xhttp.send();
 		/*exit the function:*/
